7b7aa735af39253462d98b2382b8a42e1eb0b572
gulao <gulao@huihex.com>
2020-06-03 09:44:34 +0800

    修改文件内容3

--- src/www/huihex/test/HelloWorld.java
+++ src/www/huihex/test/HelloWorld.java
@@ -5,7 +5,22 @@ package www.huihex.test;
  * @date 2020/6/3-5:00
  */
 public class HelloWorld {
+
+
     public static void main(String[] args) {
         System.out.println("hello world ！！！");
+        method1();
+    }
+    /**
+     * 测试方法
+     */
+    public static void method1(){
+        int[] array={1,22,33};
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]);
+        }
+        for (int ii : array) {
+            System.out.println(ii);
+        }
     }
 }
